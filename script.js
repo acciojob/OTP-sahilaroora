76a40c86dbeec8a45c459b6fe84a09005ccc5b62
sahilaroora <115200977+sahilaroora@users.noreply.github.com>
2025-09-17 14:46:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs[0].focus(); // Auto focus first input
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    // Accept only digits
+    if (!/^\d$/.test(value)) {
+      e.target.value = '';
+      return;
+    }
+
+    // Move to next input
+    if (index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (input.value === '') {
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = '';
+        }
+      } else {
+        input.value = '';
+      }
+    }
+  });
+});
